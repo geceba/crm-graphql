682571fef92aef343ce718d6a66772ba0c6cc9f1
Gerardo Cetzal <gerardoce23@gmail.com>
2019-04-07 21:56:17 -0500

    resolvers

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import resolvers from './resolvers';
 
+const root = resolvers;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Todo listo')
 });
-
-class Cliente {
-    constructor(id, {nombre, apellido, empresa, email}) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.empresa = empresa;
-        this.email = email;
-    }
-}
-
-const clientesDB = {};
-
-// el resolver
-const root = {
-    cliente: () => {
-        return {
-            "id": 19,
-            "nombre": "Gerardo ",
-            "apellido": "Cetzal",
-            "empresa": "BioFractal",
-            "email": "correo@es.com"
-        }
-    },
-    crearCliente: ({input}) => {
-        const id = require('crypto').randomBytes(10).toString('hex');
-        clientesDB[id] = input;
-        return new Cliente(id, input);
-
-    }
-};
 
 app.use('/graphql', graphqlHTTP({
     // schema
