9294a232eafc41e23f5a48c91a991f86f81130a1
Gerardo Cetzal <gerardoce23@gmail.com>
2019-07-30 23:29:30 -0500

    list customers

--- cliente/src/components/Customers.js
+++ cliente/src/components/Customers.js
@@ -1,19 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment } from 'react';
 import { Query } from 'react-apollo';
 import { CUSTOMERS_QUERY } from '../queries';
 
 const Customers = () => (
- <Query query={CUSTOMERS_QUERY}>
-     {({ loading, error, data }) => {
-         if (loading) return "Loading...";
-         if (error) return `Error: ${error.message}`
+	<Query query={CUSTOMERS_QUERY}>
+		{({ loading, error, data }) => {
+			if (loading) return 'Loading...';
+			if (error) return `Error: ${error.message}`;
 
-         return (
-             <h2 className="text-center">List of costumers</h2>
-         )
-     }}
+			return (
+				<Fragment>
+					<h2 className="text-center mt-4">List of costumers</h2>
+					<ul className="list-group mt-4">
+						{data.getClientes.map((item) => (
+							<li key={item.id} className="list-group-item">
+								<div className="row justify-content-between align-items-center">
+									<div className="col-md-8 justify-content-between align-items-center">
+										{item.nombre} {item.apellido} - {item.empresa}
+									</div>
 
- </Query>
+									<div className="col-md-4 d-flex justify-content-end">
+										<a className="btn btn-success d-block d-md-inline-block">Edit Customer</a>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				</Fragment>
+			);
+		}}
+	</Query>
 );
 
 export default Customers;
