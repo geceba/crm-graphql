d7f3206cef7e03e154599179800bd6df2d948e4f
Gerardo Cetzal <gerardoce23@gmail.com>
2019-08-07 23:57:33 -0500

    format

--- cliente/src/queries/index.js
+++ cliente/src/queries/index.js
@@ -1,26 +1,27 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
-
-export const CUSTOMERS_QUERY = gql`{
-  getClientes {
-    id
-    nombre
-    apellido
-    empresa
-  }
-}`;
+export const CUSTOMERS_QUERY = gql`
+	{
+		getClientes {
+			id
+			nombre
+			apellido
+			empresa
+		}
+	}
+`;
 
 export const CUSTOMER_QUERY = gql`
-  query ConsultarCliente($id: ID) {
-    getCliente(id: $id) {
-      nombre
-      apellido
-      empresa
-      edad
-      tipo
-      emails {
-        email
-      }
-    }
-  }
+	query ConsultarCliente($id: ID) {
+		getCliente(id: $id) {
+			nombre
+			apellido
+			empresa
+			edad
+			tipo
+			emails {
+				email
+			}
+		}
+	}
 `;
